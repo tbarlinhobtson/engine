viettrungluu-cr/mojo

commit e223e0c6d51f027196a2597b556641328a65d4b5

Remove -webkit-auto, -webkit-center, -webkit-left, -webkit-right.
These are values for text-align that align blocks in addition to
inline content. You can get the same centering affect with margin:auto.
If we want this functionality, we should implement it with the
same alignment APIs that we use for flexbox.

Delete unused methods/arguments from RenderBlockFlow.

No change in behavior. Just deleting dead code.

R=esprehn@chromium.org

Review URL: https://codereview.chromium.org/708843002

sky/engine/core/rendering/RenderBox.cpp
@@ -1861,9 +1861,8 @@ void RenderBox::computeMarginsForDirection(MarginDirection flowDirection, const 
 
     // CSS 2.1: "If both 'margin-left' and 'margin-right' are 'auto', their used values are equal. This horizontally centers the element
     // with respect to the edges of the containing block."
-    const RenderStyle* containingBlockStyle = containingBlock->style();
     if ((marginStartLength.isAuto() && marginEndLength.isAuto() && marginBoxWidth < containerWidth)
-        || (!marginStartLength.isAuto() && !marginEndLength.isAuto() && containingBlockStyle->textAlign() == WEBKIT_CENTER)) {
+        || (!marginStartLength.isAuto() && !marginEndLength.isAuto())) {
         // Other browsers center the margin box for align=center elements so we match them here.
         LayoutUnit centeredMarginBoxStart = std::max<LayoutUnit>(0, (containerWidth - childWidth - marginStartWidth - marginEndWidth) / 2);
         marginStart = centeredMarginBoxStart + marginStartWidth;
@@ -1878,9 +1877,7 @@ void RenderBox::computeMarginsForDirection(MarginDirection flowDirection, const 
         return;
     }
 
-    bool pushToEndFromTextAlign = !marginEndLength.isAuto() && ((!containingBlockStyle->isLeftToRightDirection() && containingBlockStyle->textAlign() == WEBKIT_LEFT)
-        || (containingBlockStyle->isLeftToRightDirection() && containingBlockStyle->textAlign() == WEBKIT_RIGHT));
-    if ((marginStartLength.isAuto() && marginBoxWidth < containerWidth) || pushToEndFromTextAlign) {
+    if (marginStartLength.isAuto() && marginBoxWidth < containerWidth) {
         marginEnd = marginEndWidth;
         marginStart = containerWidth - childWidth - marginEnd;
         return;

sky/engine/core/css/CSSPrimitiveValueMappings.h
@@ -1750,15 +1750,6 @@ template<> inline CSSPrimitiveValue::CSSPrimitiveValue(ETextAlign e)
     case JUSTIFY:
         m_value.valueID = CSSValueJustify;
         break;
-    case WEBKIT_LEFT:
-        m_value.valueID = CSSValueWebkitLeft;
-        break;
-    case WEBKIT_RIGHT:
-        m_value.valueID = CSSValueWebkitRight;
-        break;
-    case WEBKIT_CENTER:
-        m_value.valueID = CSSValueWebkitCenter;
-        break;
     }
 }
 
@@ -1766,7 +1757,6 @@ template<> inline CSSPrimitiveValue::operator ETextAlign() const
 {
     ASSERT(isValueID());
     switch (m_value.valueID) {
-    case CSSValueWebkitAuto: // Legacy -webkit-auto. Eqiuvalent to start.
     case CSSValueStart:
         return TASTART;
     case CSSValueEnd:

sky/engine/core/css/CSSValueKeywords.in
@@ -152,14 +152,10 @@ bottom
 // text-align
 // The order here must match the order of the ETextAlign enum in RenderStyleConstants.h.
 //
--webkit-auto
 left
 right
 center
 justify
--webkit-left
--webkit-right
--webkit-center
 -webkit-match-parent
 //
 // text-justify

sky/engine/core/css/parser/CSSPropertyParser.cpp
@@ -521,9 +521,9 @@ bool CSSPropertyParser::parseValue(CSSPropertyID propId, bool important)
     }
 
     case CSSPropertyTextAlign:
-        // left | right | center | justify | -webkit-left | -webkit-right | -webkit-center | -webkit-match-parent
-        // | start | end | <string> | inherit | -webkit-auto (converted to start)
-        if ((id >= CSSValueWebkitAuto && id <= CSSValueWebkitMatchParent) || id == CSSValueStart || id == CSSValueEnd
+        // left | right | center | justify | -webkit-match-parent
+        // | start | end | <string> | inherit
+        if ((id >= CSSValueLeft && id <= CSSValueWebkitMatchParent) || id == CSSValueStart || id == CSSValueEnd
             || value->unit == CSSPrimitiveValue::CSS_STRING)
             validPrimitive = true;
         break;

sky/engine/core/editing/EditingStyle.cpp
@@ -389,15 +389,12 @@ static int textAlignResolvingStartAndEnd(int textAlign, int direction)
 {
     switch (textAlign) {
     case CSSValueCenter:
-    case CSSValueWebkitCenter:
         return CSSValueCenter;
     case CSSValueJustify:
         return CSSValueJustify;
     case CSSValueLeft:
-    case CSSValueWebkitLeft:
         return CSSValueLeft;
     case CSSValueRight:
-    case CSSValueWebkitRight:
         return CSSValueRight;
     case CSSValueStart:
         return direction != CSSValueRtl ? CSSValueLeft : CSSValueRight;

sky/engine/core/rendering/RenderBlockFlow.cpp
@@ -137,9 +137,7 @@ void RenderBlockFlow::determineLogicalLeftPositionForChild(RenderBox* child)
 
     // If the child has an offset from the content edge to avoid floats then use that, otherwise let any negative
     // margin pull it back over the content edge or any positive margin push it out.
-    // If the child is being centred then the margin calculated to do that has factored in any offset required to
-    // avoid floats, so use it if necessary.
-    if (style()->textAlign() == WEBKIT_CENTER || child->style()->marginStartUsing(style()).isAuto())
+    if (child->style()->marginStartUsing(style()).isAuto())
         newPosition = std::max(newPosition, childMarginStart);
 
     child->setX(style()->isLeftToRightDirection() ? newPosition : totalAvailableLogicalWidth - newPosition - logicalWidthForChild(child));

sky/engine/core/rendering/RenderBlockLineLayout.cpp
@@ -466,15 +466,12 @@ void RenderBlockFlow::updateLogicalWidthForAlignment(const ETextAlign& textAlign
     // justifying text.
     switch (textAlign) {
     case LEFT:
-    case WEBKIT_LEFT:
         updateLogicalWidthForLeftAlignedBlock(style()->isLeftToRightDirection(), trailingSpaceRun, logicalLeft, totalLogicalWidth, availableLogicalWidth);
         break;
     case RIGHT:
-    case WEBKIT_RIGHT:
         updateLogicalWidthForRightAlignedBlock(style()->isLeftToRightDirection(), trailingSpaceRun, logicalLeft, totalLogicalWidth, availableLogicalWidth);
         break;
     case CENTER:
-    case WEBKIT_CENTER:
         updateLogicalWidthForCenterAlignedBlock(style()->isLeftToRightDirection(), trailingSpaceRun, logicalLeft, totalLogicalWidth, availableLogicalWidth);
         break;
     case JUSTIFY:

sky/engine/core/rendering/RenderBoxModelObject.cpp
@@ -2481,14 +2481,11 @@ LayoutRect RenderBoxModelObject::localCaretRectForEmptyElement(LayoutUnit width,
 
     switch (currentStyle->textAlign()) {
     case LEFT:
-    case WEBKIT_LEFT:
         break;
     case CENTER:
-    case WEBKIT_CENTER:
         alignment = alignCenter;
         break;
     case RIGHT:
-    case WEBKIT_RIGHT:
         alignment = alignRight;
         break;
     case JUSTIFY:

tbarlinhobtson/engine

commit 0868824be8560753a84e9eef65870e88062c788b

Make fixed-width elements not center by default.
e223e0c6d51f027196a2597b556641328a65d4b5 accidentally
sent all elements down the margin:auto codepath.
The patch incorrectly removed just the
"&& containingBlockStyle->textAlign() == WEBKIT_CENTER"
instead of the whole clause.

R=abarth@chromium.org

Review URL: https://codereview.chromium.org/760143002

engine/core/rendering/RenderBox.cpp
@@ -1647,8 +1647,7 @@ void RenderBox::computeMarginsForDirection(MarginDirection flowDirection, const 
 
     // CSS 2.1: "If both 'margin-left' and 'margin-right' are 'auto', their used values are equal. This horizontally centers the element
     // with respect to the edges of the containing block."
-    if ((marginStartLength.isAuto() && marginEndLength.isAuto() && marginBoxWidth < containerWidth)
-        || (!marginStartLength.isAuto() && !marginEndLength.isAuto())) {
+    if (marginStartLength.isAuto() && marginEndLength.isAuto() && marginBoxWidth < containerWidth) {
         // Other browsers center the margin box for align=center elements so we match them here.
         LayoutUnit centeredMarginBoxStart = std::max<LayoutUnit>(0, (containerWidth - childWidth - marginStartWidth - marginEndWidth) / 2);
         marginStart = centeredMarginBoxStart + marginStartWidth;

tests/layout/ellipsis-expected.txt
@@ -2,20 +2,20 @@ layer at (0,0) size 800x600
   RenderView {#document} at (0,0) size 800x600
 layer at (0,0) size 800x94
   RenderBlock {sky} at (0,0) size 800x94
-layer at (372,0) size 56x69 clip at (375,3) size 50x63 scrollWidth 93
-  RenderBlock {div} at (372,0) size 56x69 [border: (3px solid #0000FF)]
+layer at (0,0) size 56x69 clip at (3,3) size 50x63 scrollWidth 93
+  RenderBlock {div} at (0,0) size 56x69 [border: (3px solid #0000FF)]
     RenderBlock (anonymous) at (3,3) size 50x19
       RenderText {#text} at (0,0) size 91x19
         text run at (0,0) width 91: "These lines"
     RenderBlock (anonymous) at (3,47) size 50x19
       RenderText {#text} at (0,0) size 94x19
         text run at (0,0) width 94: "all ellipsize."
-layer at (375,22) size 56x25 backgroundClip at (375,22) size 50x25 clip at (378,25) size 47x19 outlineClip at (375,3) size 50x63 scrollWidth 53
+layer at (3,22) size 56x25 backgroundClip at (3,22) size 50x25 clip at (6,25) size 47x19 outlineClip at (3,3) size 50x63 scrollWidth 53
   RenderBlock {p} at (3,22) size 56x25 [border: (3px solid #FF0000)]
     RenderText {#text} at (3,3) size 54x19
       text run at (3,3) width 54: "should"
-layer at (372,69) size 56x25 clip at (375,72) size 50x19 scrollWidth 117
-  RenderBlock {p} at (372,69) size 56x25 [border: (3px solid #FF0000)]
+layer at (0,69) size 56x25 clip at (3,72) size 50x19 scrollWidth 117
+  RenderBlock {p} at (0,69) size 56x25 [border: (3px solid #FF0000)]
     RenderInline {span} at (0,0) size 117x19
       RenderText {#text} at (3,3) size 117x19
         text run at (3,3) width 117: "As should this."

tests/layout/fixed-width-expected.txt
@@ -1,0 +1,6 @@
+layer at (0,0) size 800x600
+  RenderView {#document} at (0,0) size 800x600
+layer at (0,0) size 800x50
+  RenderBlock {sky} at (0,0) size 800x50
+    RenderBlock {div} at (0,0) size 50x50 [bgcolor=#FFC0CB]
+

tests/layout/fixed-width.sky
@@ -1,0 +1,4 @@
+<sky>
+  <import src="../resources/dump-as-render-tree.sky" />
+  <div style="width: 50px; height: 50px; background-color: pink;"></div>
+</sky>

tests/lowlevel/iframe-expected.txt
@@ -20,8 +20,8 @@ layer at (0,0) size 800x506
       RenderBlock (anonymous) at (0,59) size 800x19
         RenderText {#text} at (0,0) size 76x19
           text run at (0,0) width 76: "correctly."
-layer at (230,19) size 340x190
-  RenderReplaced {iframe} at (230,19) size 340x190 [border: (20px solid #000000)]
-layer at (250,447) size 300x40
-  RenderReplaced {iframe} at (250,19) size 300x40
+layer at (0,19) size 340x190
+  RenderReplaced {iframe} at (0,19) size 340x190 [border: (20px solid #000000)]
+layer at (0,447) size 300x40
+  RenderReplaced {iframe} at (0,19) size 300x40
 

tests/lowlevel/img-expected.txt
@@ -2,6 +2,6 @@ layer at (0,0) size 800x600
   RenderView {#document} at (0,0) size 800x600
 layer at (0,0) size 800x89
   RenderBlock {foo} at (0,0) size 800x89
-    RenderImage {img} at (272,0) size 256x64
-    RenderImage {img} at (350,64) size 100x25
+    RenderImage {img} at (0,0) size 256x64
+    RenderImage {img} at (0,64) size 100x25
 